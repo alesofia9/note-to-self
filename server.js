d836e4a09c2ea4bfa6bb0339d2a75b5fb5d75a05
alesofia9 <alesofia09@gmail.com>
2023-11-07 12:09:47 -0600

    Removed 'Develop' folder, edited, routes, need to tweak post function.'' git push

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
+const util = require('util'); //maybe can delete...// 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-//Express App//
+//Express App...//
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -13,29 +13,24 @@ app.use(express.urlencoded({extended: true }));
 
 app.use(express.json());
 
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
 
-//HTML// 
+//HTML and API routes...// 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-//May get rid off later.//
+
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-//Listening//
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
 
 app.get('/api/notes', function(req, res) {
-    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data) {
+    readFileAsync('./db/db.json', 'utf8').then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
@@ -43,15 +38,19 @@ app.get('/api/notes', function(req, res) {
 
 app.post('/api/notes', function(req, res) {
     const note = req.body;
-    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data) {
+    readFileAsync('./db/db.json', 'utf8').then(function(data) {
         const notes = [].concat(JSON.parse(data));
         note.id = notes.length + 1
         notes.push(note);
         return notes
     }).then(function(notes) {
-        writeFileAsync('./Develop/db/db.json', JSON.stringify(notes))
+        writeFileAsync('./db/db.json', JSON.stringify(notes))
         res.json(note);
     })
 });
 
+//Listening//
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
 //Fix POST server. Get request to save html file, and possbily fix the buttons? // 
